dd5a7cd9dafd62a6d492ee84a42f0ec827969462
Miguel-ectil <ectilmiguel@gmail.com>
2025-11-07 01:11:23 -0300

    feat: [UPD] Configurando o cors para interagir com o front-end

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from src.config.data_base import init_db, db
 from src.routes import init_routes
 from src.Infrastructure.Model.user import User  # Caminho corrigido do seu model
+
 
 def create_app():
     """
@@ -9,7 +11,10 @@ def create_app():
     """
     app = Flask(__name__)
     init_db(app)
-    init_routes(app)
+    init_routes(app)  
+
+    CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
+
     return app
 
 app = create_app()
@@ -18,4 +23,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
