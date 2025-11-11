4a93a59509c9e8bcb0c6bdd5ab92b894eb7a38f1
Miguel-ectil <ectilmiguel@gmail.com>
2025-11-11 00:24:59 -0300

    feat: [UPD] Adicioando mais dados de reorno e envio para o produto

--- src/config/data_base.py
+++ src/config/data_base.py
@@ -8,11 +8,8 @@ def init_db(app):
     Inicializa a base de dados com o app Flask e o SQLAlchemy.
     Utiliza SQLite como banco de dados local.
     """
-    # basedir = os.path.abspath(os.path.dirname(__file__))
-    # sqlite_path = os.path.join(basedir, '../../database.sqlite')  # Ajuste o caminho se necessário
-
-    sqlite_path = '/app/src/database.sqlite'
-
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    sqlite_path = os.path.join(basedir, '../../database.sqlite')  
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{sqlite_path}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
