ef9b52eed61d2c06e54f1d229dfc5cc2d7380f1a
Miguel-ectil <ectilmiguelmiguelectil.com>
2025-11-11 23:25:48 -0300

    feat: [UPD] Atualizando o config removendo sqLite passando o postgres do supabase atualizando .env atualizando arquivo run

--- run.py
+++ run.py
@@ -7,25 +7,21 @@ from src.Infrastructure.Model.produto import Produto
 from src.Infrastructure.Model.venda import Venda
 
 def create_app():
-    """
-    Função que cria e configura a aplicação Flask.
-    """
     app = Flask(__name__)
     init_db(app)
-    init_routes(app)  
+    init_routes(app)
 
     CORS(
         app,
         resources={r"/*": {"origins": ["http://localhost:3000", "https://gest-stock-front.vercel.app"]}},
         supports_credentials=True
     )
-
     return app
 
 app = create_app()
 
 with app.app_context():
-    db.create_all()
+    db.create_all() 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8000)
